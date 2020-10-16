b4c3319ef11cede30779e63acbb0ebb6ed678c38
Jesse Wang <jessewang@91app.com>
2020-10-16 18:46:48 +0800

    Type exam

---
+++ Type/Exam1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = "{\r\n\t\"StockQty\": 10,\r\n\t\"SafetyStockQty\": \"\",\r\n\t\"UpdatedUser\": \"David\"\r\n}";
+
+            var stock = JsonConvert.DeserializeObject<Stock>(json);
+
+            //// How to Fixed this exception?
+            /*
+             * Newtonsoft.Json.JsonSerializationException: 
+             * 'Error converting value {null} to type 'System.Int32'. Path 'SafetyStockQty', line 3, position 21.'
+             */
+            Console.WriteLine($"The default safety stock:{stock.SafetyStockQty}");
+        }
+    }
+}
---
+++ Type/Exam1/Stock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam1
+{
+    public class Stock
+    {
+        public int StockQty { get; set; }
+
+        public int SafetyStockQty { get; set; }
+
+        public string UpdatedUser { get; set; }
+    }
+}
